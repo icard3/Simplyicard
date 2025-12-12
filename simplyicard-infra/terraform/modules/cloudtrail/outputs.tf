10eeadc5f87b6259a6728400495559f768080943
Ravi Teja k <rangarao364@gmail.com>
2025-12-12 13:02:40 +0530

    feat: scaffold security modules

---
+++ simplyicard-infra/terraform/modules/cloudtrail/outputs.tf
@@ -0,0 +1,4 @@
+output "trail_arn" {
+  description = "ARN of the CloudTrail"
+  value       = "" # Placeholder
+}
---
+++ simplyicard-infra/terraform/modules/cloudtrail/variables.tf
@@ -0,0 +1,5 @@
+variable "s3_bucket_name" {
+  description = "S3 bucket for CloudTrail logs"
+  type        = string
+  default     = "" # Optional/Placeholder
+}
