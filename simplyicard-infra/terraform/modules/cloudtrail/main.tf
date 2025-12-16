32f05915986163190bf3322d9a2f35597356dd40
Srinu Matta <srinumatta933@gmail.com>
2025-12-16 16:06:12 +0530

    implement cloudtrail module

--- simplyicard-infra/terraform/modules/cloudtrail/main.tf
+++ simplyicard-infra/terraform/modules/cloudtrail/main.tf
@@ -1,3 +1,73 @@
-# TODO: Configure CloudTrail here
-# - aws_cloudtrail
-# - aws_s3_bucket (for logs)
+# S3 bucket for CloudTrail logs
+resource "aws_s3_bucket" "cloudtrail_logs" {
+  bucket = var.s3_bucket_name
+}
+resource "aws_s3_bucket_lifecycle_configuration" "cloudtrail_logs" {
+  bucket = aws_s3_bucket.cloudtrail_logs.id
+
+  rule {
+    id     = "log-expiration"
+    status = "Enabled"
+
+    filter {
+      prefix = "AWSLogs/"
+    }
+
+    expiration {
+      days = 90
+    }
+  }
+}
+
+
+# Bucket policy to allow CloudTrail to write logs
+resource "aws_s3_bucket_policy" "cloudtrail_logs_policy" {
+  bucket = aws_s3_bucket.cloudtrail_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailAclCheck"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "s3:GetBucketAcl"
+        Resource  = aws_s3_bucket.cloudtrail_logs.arn
+      },
+      {
+        Sid       = "AWSCloudTrailWrite"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.cloudtrail_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# CloudTrail configuration
+resource "aws_cloudtrail" "main" {
+  name                          = "cloudtrail-${var.s3_bucket_name}"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail_logs.id
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_log_file_validation    = true
+
+  event_selector {
+    read_write_type           = "All"
+    include_management_events = true
+  }
+
+  tags = {
+    Name        = "cloudtrail-${var.s3_bucket_name}"
+    Environment = var.s3_bucket_name
+  }
+}
+
+# Helpful: get current account ID for bucket policy
+data "aws_caller_identity" "current" {}
