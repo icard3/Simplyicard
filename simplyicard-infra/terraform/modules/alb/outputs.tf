6410487776b8bea1cc1f23dd686a86a6a46d2cad
saitejabatti <saitejabatti@gmail.com>
2025-12-18 15:44:00 +0530

    Fix ALB name interpolation and resource outputs

--- simplyicard-infra/terraform/modules/alb/outputs.tf
+++ simplyicard-infra/terraform/modules/alb/outputs.tf
@@ -1,16 +1,16 @@
 output "alb_dns_name" {
   description = "The DNS name of the ALB"
-  value       = "aws_lb.alb.dns_name"
+  value       = aws_lb.alb.dns_name
 }
 
 output "alb_zone_id" {
   description = "The Zone ID of the ALB"
-  value       = "aws_lb.alb.zone_id" # Placeholder
+  value       = aws_lb.alb.zone_id # Placeholder
 }
 
 output "target_group_arn" {
   description = "ARN of the default target group"
-  value       = "aws_lb_target_group.tg.arn" # Placeholder
+  value       = aws_lb_target_group.tg.arn # Placeholder
 }
 # - alb_dns_name
 # - target_group_arn
