92da2143ae3b1a4b58c4b109c753950355a60def
Srinu Matta <srinumatta933@gmail.com>
2025-12-18 18:12:05 +0530

    add initial AWS SecurityHub module implementation

--- simplyicard-infra/terraform/modules/securityhub/main.tf
+++ simplyicard-infra/terraform/modules/securityhub/main.tf
@@ -1,2 +1,24 @@
-# TODO: Configure Security Hub here
-# - aws_securityhub_account
+# Enable Security Hub in the account
+resource "aws_securityhub_account" "this" {
+  auto_enable_controls = true
+}
+
+# Always subscribe to AWS Foundational Security Best Practices
+resource "aws_securityhub_standards_subscription" "foundational" {
+  standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/aws-foundational-security-best-practices/v/1.0.0"
+}
+
+# Conditionally subscribe to CIS AWS Foundations Benchmark
+resource "aws_securityhub_standards_subscription" "cis" {
+  count         = var.enable_cis_benchmark ? 1 : 0
+  standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/cis-aws-foundations-benchmark/v/1.4.0"
+}
+
+# Optionally configure delegated admin account for org-wide Security Hub
+resource "aws_securityhub_organization_admin_account" "admin" {
+  count            = var.delegated_admin_account_id != "" ? 1 : 0
+  admin_account_id = var.delegated_admin_account_id
+}
+
+# Data source for current region
+data "aws_region" "current" {}
--- simplyicard-infra/terraform/modules/securityhub/variables.tf
+++ simplyicard-infra/terraform/modules/securityhub/variables.tf
@@ -1,1 +1,12 @@
-# No variables required for basic enablement
+## No variables required for basic enablement
+variable "enable_cis_benchmark" {
+  description = "Enable CIS AWS Foundations Benchmark standard in SecurityHub"
+  type        = bool
+  default     = true
+}
+
+variable "delegated_admin_account_id" {
+  description = "Delegated SecurityHub admin account ID"
+  type        = string
+  default     = ""
+}
