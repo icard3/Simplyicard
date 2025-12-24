11b82fb53614af3f20e82a308f2c5542d74c6d16
Ravi Teja k <rangarao364@gmail.com>
2025-12-24 21:30:54 +0530

    fix: update ALB health check path to /health

--- simplyicard-infra/terraform/modules/alb/main.tf
+++ simplyicard-infra/terraform/modules/alb/main.tf
@@ -55,7 +55,7 @@ resource "aws_lb_target_group" "tg" {
   target_type = "ip"
 
   health_check {
-    path                = "/"
+    path                = "/health"
     protocol            = "HTTP"
     port                = "traffic-port"
     healthy_threshold   = 3
